3e8f288199932ea45d018dfa03567bccd266e90b
Valentina Korotkova <korobok.valek@mail.ru>
2023-06-06 20:45:48 +0600

    Первое задание

---
+++ DZ5/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите размер массива");
+int length = int.Parse(Console.ReadLine());
+int[] Arr = new int[length];
+FillArrayRandomNumbers(Arr);
+Console.WriteLine("Сгененрированный массив: ");
+PrintArray(Arr);
+int count = 0;
+
+for (int z = 0; z < Arr.Length; z++)
+if (Arr[z] % 2 == 0)
+count++;
+Console.WriteLine($"{count} - чётные числа");
+
+void FillArrayRandomNumbers(int[] Arr)
+{
+    for(int i = 0; i < Arr.Length; i++)
+    {
+        Arr[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] Arr)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < Arr.Length; i++)
+    {
+        Console.Write(Arr[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
